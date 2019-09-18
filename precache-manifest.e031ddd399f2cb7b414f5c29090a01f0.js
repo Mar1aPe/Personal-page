18050b66aca07bfb689bc733494e564e1a14c28f
Mar1aPe <adresmarii@gmail.com>
2019-09-18 21:45:46 +0200

    Updates

---
+++ precache-manifest.e031ddd399f2cb7b414f5c29090a01f0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c358edd317071ce0de10fd7edc8a1d2b",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "d8788a41e0d0f172ae90",
+    "url": "/portfolio/static/css/main.0d6ec7de.chunk.css"
+  },
+  {
+    "revision": "7c7d167f47606dc5a25f",
+    "url": "/portfolio/static/js/2.d56e15df.chunk.js"
+  },
+  {
+    "revision": "d8788a41e0d0f172ae90",
+    "url": "/portfolio/static/js/main.69217317.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "17c434d632a93d8a06909ef1d1230113",
+    "url": "/portfolio/static/media/derpina.17c434d6.png"
+  },
+  {
+    "revision": "27ce0367360bc1b224c78ba2cb9af45f",
+    "url": "/portfolio/static/media/sky.27ce0367.jpg"
+  },
+  {
+    "revision": "34f4e010cb8589b420d390c9181adbc6",
+    "url": "/portfolio/static/media/tetris.34f4e010.png"
+  }
+]);
